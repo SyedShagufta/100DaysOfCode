7467273e380a92583193d61931891fbf5dfeafc2
SyedShagufta <sweetyvox123@gmail.com>
2024-01-21 20:03:06 +0530

    DAY-46

--- DAY-46/main.py
+++ DAY-46/main.py
@@ -1,5 +1,11 @@
+import os
 import requests
+import spotipy
 from bs4 import BeautifulSoup
+from spotipy.oauth2 import SpotifyOAuth
+
+SPOTIPY_CLIENT_ID = os.environ["SPOTIPY_CLIENT_ID"]
+SPOTIPY_CLIENT_SECRET = os.environ["SPOTIPY_CLIENT_SECRET"]
 # input the year in the format YYYY-MM-DD
 year = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD")
 
@@ -12,3 +18,19 @@ soup = BeautifulSoup(web_page, "html.parser")
 songs = soup.select("li ul li h3")
 songs_titles = [song.getText().strip() for song in songs]
 print(songs_titles)
+
+# Authorizing to spotify
+scope = "user-library-read user-read-playback-state user-read-currently-playing"
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope=scope,
+        redirect_uri="http://example.com",
+        client_id=SPOTIPY_CLIENT_ID,
+        client_secret=SPOTIPY_CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt",
+        username="wtwiwyjv6iqlgcbok1sbo3m4e",
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)
