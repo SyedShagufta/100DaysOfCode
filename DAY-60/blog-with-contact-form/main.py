14011324b5a81aade0a8ad747b290db9c37f8c81
SyedShagufta <sweetyvox123@gmail.com>
2024-06-13 23:02:35 +0530

    DAY - 60

---
+++ DAY-60/blog-with-contact-form/main.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import requests
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/18f49b80c208c8ea9e22").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "GET":
+        return render_template("contact.html", msg_sent=False)
+    else:
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+        print(f"{name} \n {email} \n {phone} \n {message}")
+        return render_template("contact.html", msg_sent=True)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
