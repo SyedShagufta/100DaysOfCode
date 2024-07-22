474de4f9b8d9a4ba03c817b6f045aafd8533bb2c
SyedShagufta <sweetyvox123@gmail.com>
2024-07-22 22:41:57 +0530

    DAY - 63 - SQLAlchemy Demo

---
+++ DAY-63/SQLAlchemy_Demo/main.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    email: Mapped[str]
+
+
+# create the app
+app = Flask(__name__)
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# initialize the app with the extension
+db.init_app(app)
+
+
+with app.app_context():
+    db.create_all()
+
