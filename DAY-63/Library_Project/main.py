ef1b08664d31ca5f5e8b7a6046c8464838d04df9
SyedShagufta <sweetyvox123@gmail.com>
2024-07-23 16:21:13 +0530

    DAY - 63 - Get Method

--- DAY-63/Library_Project/main.py
+++ DAY-63/Library_Project/main.py
@@ -1,29 +1,68 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 app = Flask(__name__)
 
-all_books = []
+
+# Create database
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///library-books.db"
+
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# Create the table
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    # This will allow each book object to be identified by its title when printed
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+# Create table schema in the database. Requires application context
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def home():
-    return render_template('index.html',  all_books=all_books, length=len(all_books))
+    all_books = db.session.execute(db.select(Book).order_by(Book.title)).scalars()
+    return render_template('index.html', all_books=all_books)
 
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
-        new_book = {
-            "title": request.form["name"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
+        title = request.form["name"]
+        author = request.form["author"]
+        rating = request.form["rating"]
+        new_book = Book(title=title, author=author, rating=rating)
+        db.session.add(new_book)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template('add.html')
 
 
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    print(id)
+    if request.method == 'POST':
+        book_to_update = db.session.execute(db.select(Book).where(Book.id == id)).scalar()
+        book_to_update.rating = request.form["new_rating"]
+        return redirect(url_for('home'))
+    return render_template('edit.html', id=id)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
